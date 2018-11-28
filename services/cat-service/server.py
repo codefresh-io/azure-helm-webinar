b4e55e162b09385c125cccd090f0ae7cafab6cdc
Josh Dolitsky <jdolitsky@gmail.com>
2018-11-28 05:18:34 -0600

    random cats

--- services/cat-service/server.py
+++ services/cat-service/server.py
@@ -1,14 +1,25 @@
 import os
+import random
 import BaseHTTPServer
 
 PORT = os.getenv('PORT', 8080)
+
+cats = [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz7DnepEt9HKWgFwpdU1U-EkBh_BVyGyl_L2xBA8Maaa3mTQ5E',
+    'https://thepawington.com/wp-content/uploads/2014/02/kitten.png',
+    'http://wall2born.com/data/out/579/image-49251716-cute-kitten-pics.jpg',
+    'https://ih0.redbubble.net/image.474690746.7074/ap,550x550,12x16,1,transparent,t.u1.png',
+    'https://cat-bounce.com/catbounce.png'
+]
 
 class CustomHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write('{"cat": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz7DnepEt9HKWgFwpdU1U-EkBh_BVyGyl_L2xBA8Maaa3mTQ5E"}')
+        #cat = cats[0]
+        cat = random.choice(cats)
+        self.wfile.write('{"cat": "%s"}' % cat)
 
 def main():
     server = BaseHTTPServer.HTTPServer(('', PORT), CustomHandler)
